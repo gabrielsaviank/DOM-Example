ac825a08fa0f3d49bfbb64da3dd3eab2c13ae45d
Gabriel Savian <gabrielsvnzrd@gmail.com>
2020-03-18 15:15:13 -0300

    added JS

--- myscript.js
+++ myscript.js
@@ -14,6 +14,22 @@ function clearBoard() {
 }
 restart.addEventListener('click',clearBoard)
 
-// Check the square marker
 
-// For Loop to add event listeners to all the squares
+
+
+// Create a function that will check the square marker
+function changeMarker(){
+    if(this.textContent === ''){
+      this.textContent = 'X';
+      // console.log(marker)
+    }else if (this.textContent === 'X') {
+      this.textContent = 'O';
+    }else {
+      this.textContent = '';
+    }
+};
+
+// Use a for loop to add Event listeners to all the squares
+for (var i = 0; i < squares.length; i++) {
+    squares[i].addEventListener('click', changeMarker);
+}
