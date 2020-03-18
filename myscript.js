8119cd2675c047fc01d381bbd56cbfe8097e2f0e
Gabriel Savian <gabrielsvnzrd@gmail.com>
2020-03-18 14:50:41 -0300

    Addes JS

--- myscript.js
+++ myscript.js
@@ -1,1 +1,19 @@
-console.log('Connected');
+// Restart Game Button
+var restart = document.querySelector('#b');
+
+// Grab all the squares
+var squares = document.querySelectorAll("td");
+
+
+// Clear Squares Function
+function clearBoard() {
+  for (var i = 0; i < squares.length; i++) {
+      squares[i].textContent = '';
+  }
+
+}
+restart.addEventListener('click',clearBoard)
+
+// Check the square marker
+
+// For Loop to add event listeners to all the squares
